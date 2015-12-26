18e53b9f2d593be4e08aea83f22751044f0011ef
Rodrigo Valin <licorna@gmail.com>
2015-12-26 01:12:01 -0300

    Exercise 2.59

--- section2.3.rkt
+++ section2.3.rkt
@@ -112,3 +112,13 @@
          (cons (car set1) intersection-set (cdr set1) set2))
         (else intersection-set (cdr set1) set2)))
 
+;; exercise 2.59
+;;
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((not (element-of-set? (car set1) set2))
+         (cons (car set1) (union-set (cdr set1) set2)))
+        (else (union-set (cdr set1) set2))))
+
