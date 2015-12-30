58ff6d9ef99caa8e731af0473fbdc8432699dabf
Rodrigo Valin <licorna@gmail.com>
2015-12-30 20:34:25 -0300

    Advances.

--- section_2_4_1.rkt
+++ section_2_4_1.rkt
@@ -23,3 +23,19 @@
    (/ (magnitude z1) (magnitude z2))
    (- (angle z1) (angle z2))))
 
+(define (real-part z) (car z))
+(define (imag-part z) (cdr z))
+
+(define (magnitude z)
+  (sqrt (+ (square (real-part z))
+           (square (imag-part z)))))
+
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+
+(define (make-from-real-imag x y)
+  (cons x y))
+
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a)) (* r (sin a))))
+
