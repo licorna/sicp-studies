5042968728c6699720231272a01a7b80a6d572f1
Rodrigo Valin <licorna@gmail.com>
2015-12-30 20:30:20 -0300

    Starting new section 2.4.

---
+++ section_2_4_1.rkt
@@ -0,0 +1,25 @@
+;; -*- geiser-scheme-implementation: guile -*-
+
+;; Section 2.4.1
+;;
+
+(define (add-complex z1 z2)
+  (make-from-real-imag
+   (+ (real-part z1) (real-part z2))
+   (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-real-imag
+   (- (real-part z1) (real-part z2))
+   (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-real-imag
+   (* (magnitude z1) (magnitude z2))
+   (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-real-imag
+   (/ (magnitude z1) (magnitude z2))
+   (- (angle z1) (angle z2))))
+
