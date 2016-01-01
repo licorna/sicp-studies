df93a47d3f3f6172e689a704f226b65ded85be62
Rodrigo Valin <licorna@gmail.com>
2016-01-01 13:48:35 -0300

    Starting ex. 2.73

--- section_2_4_1.rkt
+++ section_2_4_1.rkt
@@ -173,3 +173,49 @@
 (define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar r a)))
 
+
+
+;;
+;; Exercise 2.73
+;;
+
+(define (install-sum-package)
+  ;; selectors
+  (define (addend s) (cadr s))
+  (define (augend s) (caddr s))
+  ;; constructor
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2))
+           (+ a1 a2))
+          (else (list '+ a1 a2))))
+  ;; deriv
+  (define (deriv exp var)
+    (make-sum (deriv (addend exp) var)
+              (deriv (augend exp) var)))
+  'done)
+
+(define (install-product-package)
+  ;; selectors
+  (define (multiplier p) (cadr p))
+  (define (multiplicand p) (caddr p))
+  ;; constructor
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0)
+               (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2))
+           (* m1 m2))
+          (else (list '* m1 m2))))
+  ;; deriv
+  (define (deriv exp var)
+    (make-sum
+     (make-product
+      (multiplier exp)
+      (deriv (multiplicand exp) var))
+     (make-product
+      (deriv (multiplier exp) var)
+      (multiplicand exp))))
+  'done)
