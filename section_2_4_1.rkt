0ddf2ecece7ea3197bb226f6abfe3dc10461caa2
Rodrigo Valin <licorna@gmail.com>
2016-01-01 14:31:30 -0300

    Exercise 2.75

--- section_2_4_1.rkt
+++ section_2_4_1.rkt
@@ -228,3 +228,29 @@
 
 (define (deriv exp var)
   (apply-generic 'deriv exp var))
+
+
+;;
+;; Exercise 2.75
+;;
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(define (make-from-mag-angle r a)
+  (define (dispatch op)
+    (cond ((eq? op 'magnitude) r)
+          ((eq? op 'angle) a)
+          ((eq? op 'real-part)
+           (* r (cos a)))
+          ((eq? op 'imag-part)
+           (* r (sin a)))
+          (else (error "Unknown op: MAKE-FROM-MAG-ANGLE" op)))))
+
