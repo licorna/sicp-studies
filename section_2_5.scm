2cad54f0bd8becf304e119ef7490ff7b9d19e48f
Rodrigo Valin <licorna@gmail.com>
2016-01-02 11:05:17 -0300

    Added generic methods.

---
+++ section_2_5.scm
@@ -0,0 +1,20 @@
+;; -*- geiser-scheme-implementation: guile -*-
+
+;; Section 2.5
+;;
+;; Rodrigo Valin <licorna@gmail.com>
+;; Jan. 2016
+;;
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
