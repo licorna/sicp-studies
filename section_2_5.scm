f13b10fc0ad8726ac1f537177651c324d53f1777
Rodrigo Valin <licorna@gmail.com>
2016-01-02 11:52:07 -0300

    Rational package.

--- section_2_5.scm
+++ section_2_5.scm
@@ -22,6 +22,8 @@
 (define (attach-tag tag symbol)
   (cons tag symbol))
 
+
+;; Scheme number Package
 (define (install-scheme-number-package)
   (define (tag x)
     (attach-tag 'scheme-number x))
@@ -36,5 +38,46 @@
   (put 'make 'scheme-number
        (lambda (x) (tag x)))
   'done)
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
 
 
+;; Rational package
+(define (install-rational-package)
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag add-rat x y)))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag sub-rat x y)))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag mul-rat x y)))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag div-rat x y)))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+  
+    
+                 
